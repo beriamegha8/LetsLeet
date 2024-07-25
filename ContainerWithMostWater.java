aefbbb32046a38a0a84c51d5519ececcb0eeb2b6
Megha Beria <122794069+beriamegha8@users.noreply.github.com>
2024-07-25 14:22:56 +0530

    Create ContainerWithMostWater.java
    62/62 passed, 5ms runtime

---
+++ ContainerWithMostWater.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int maxArea(int[] height) {
+        int max = 0;
+        int left = 0;
+        int right = height.length - 1;
+        while (left < right) {
+            int minHeight = Math.min(height[left], height[right]);
+            int area = minHeight * (right - left);
+            max = Math.max(max, area);
+            if (height[left] < height[right]) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+        return max;
+    }
+}
