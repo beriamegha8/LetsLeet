b54830d1eefad850fd8bd26de290374ceee694fe
Megha Beria <122794069+beriamegha8@users.noreply.github.com>
2024-08-22 23:21:38 +0530

    Create DetermineIfTwoStringsAreClose.java
    169/169 passed, 9ms runtime

---
+++ DetermineIfTwoStringsAreClose.java
@@ -0,0 +1,32 @@
+class Solution {
+    public boolean closeStrings(String word1, String word2) {
+        if (word1.length() != word2.length()) {
+            return false;
+        }
+
+        int[] freq1 = new int[26];
+        int[] freq2 = new int[26];
+        
+        //count frequencies of characters in word1 and word2
+        for (char c : word1.toCharArray()) {
+            freq1[c - 'a']++;
+        }
+        for (char c : word2.toCharArray()) {
+            freq2[c - 'a']++;
+        }
+        
+        //to check if both strings have the same unique characters
+        for (int i = 0; i < 26; i++) {
+            if ((freq1[i] == 0 && freq2[i] != 0) || (freq1[i] != 0 && freq2[i] == 0)) {
+                return false;
+            }
+        }
+        
+        //sort the frequency arrays to compare them
+        Arrays.sort(freq1);
+        Arrays.sort(freq2);
+        
+        //if the sorted frequency arrays are equal
+        return Arrays.equals(freq1, freq2);
+    }
+}
