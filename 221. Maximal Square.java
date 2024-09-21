8b23e8010b27398f29017b567f8f88fc87e3132b
Megha Beria <122794069+beriamegha8@users.noreply.github.com>
2024-09-22 00:01:31 +0530

    Create 221. Maximal Square.java
    78/78 passed, 7ms runtime

---
+++ 221. Maximal Square.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int maximalSquare(char[][] matrix) {
+        if (matrix == null || matrix.length == 0 || matrix[0].length == 0) {
+            return 0;
+        }
+        int maxSide = 0;
+        int m = matrix.length;
+        int n = matrix[0].length;
+        int[][] dp = new int[m][n];
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (matrix[i][j] == '1') {
+                    if (i == 0 || j == 0) {
+                        dp[i][j] = 1; // Only 1 can form a square
+                    } else {
+                        dp[i][j] = Math.min(dp[i-1][j], Math.min(dp[i][j-1], dp[i-1][j-1])) + 1;
+                    }
+                    maxSide = Math.max(maxSide, dp[i][j]);
+                }
+            }
+        }
+
+        return maxSide * maxSide; 
+    }
+}
